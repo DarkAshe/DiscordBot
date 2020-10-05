ec0e2e33e94d87483c8718674aeaef691f270b8d
DarkAshe <gavriloviustin2016@gmail.com>
2020-10-05 20:18:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -193,39 +193,8 @@ client.on('message', async message => {
     const args = message.content.substring(PREFIX.length).split(" ")
     const command = args.shift().toLowerCase()
 
-    if(command === 'image'){
-        switch (args[0]) {
-            case 'image':
-            image(message)
-            break
-        }
-        function image(message){
- 
-            var options = {
-                url: "http://results.dogpile.com/serp?qc=images&q=" + "Trump",
-                method: "GET",
-                headers: {
-                    "Accept": "text/html",
-                    "User-Agent": "Chrome"
-                }
-            };
-        
-            request(options, function(error, response, responseBody) {
-         
-                $ = cheerio.load(responseBody);
-         
-                var links = $(".image a.link");
-         
-                var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-               
-                console.log(urls);
-         
-                // Send result
-                message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-            });
-         
-        }
-    }
+    if(command === 'clear'){
+        client.commands.get('clear').execute(message, args)
 });
 
 client.login(process.env.token)
