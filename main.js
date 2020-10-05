6cff79d4a8bcc4f377360511ba7c179abd7a457d
DarkAshe <gavriloviustin2016@gmail.com>
2020-10-05 08:21:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -168,23 +168,77 @@ client.on("guildMemberAdd", member => {
 })
 
 client.on('message', async message => {
-    if(message.author.bot) return;
+    if(message.author.bot) return
     if(message.channel.id === '755102491246592040')
-        await message.delete();
+        await message.delete()
     if(message.content.toLowerCase() === '!verify' && message.channel.id === '755102491246592040')
     {   
-        await message.delete().catch(err => console.log(err));
-        const role = message.guild.roles.cache.get('755087485687562350');
+        await message.delete().catch(err => console.log(err))
+        const role = message.guild.roles.cache.get('755087485687562350')
         if(role) {
             try {
-                await message.member.roles.add(role);
+                await message.member.roles.add(role)
                 console.log("Role added!");
             }
             catch(err) {
-                console.log(err);
+                console.log(err)
             }
         }
     }
-});
+})
+
+client.on('message', message =>{
+    if(!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'demomesa'){
+       client.commands.get('demomesa').execute(message, args);
+    }
+    if(command === 'clear'){
+       client.commands.get('clear').execute(message, args);
+    }
+    if(command === 'stfu'){
+        client.commands.get('stfu').execute(message, args);
+    }
+    if(command === 'osama'){
+        client.commands.get('osama').execute(message, args);
+    }
+    if(command === 'image'){
+        let args = message.content.substring(prefix.length).split(" ");
+        switch (args[0]) {
+            case 'image':
+            image(message);
+            break;
+        }
+        function image(message){
+ 
+            var options = {
+                url: "http://results.dogpile.com/serp?qc=images&q=" + "memes",
+                method: "GET",
+                headers: {
+                    "Accept": "text/html",
+                    "User-Agent": "Chrome"
+                }
+            };
+        
+            request(options, function(error, response, responseBody) {
+         
+                $ = cheerio.load(responseBody)
+         
+                var links = $(".image a.link")
+         
+                var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"))
+               
+                console.log(urls)
+         
+                // Send result
+                message.channel.send( urls[Math.floor(Math.random() * urls.length)])
+            })
+         
+        }
+    }
+})
 
 client.login(process.env.token)
